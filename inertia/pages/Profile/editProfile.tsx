d60e24adb95095372316dbb1d8fccfe8c652bea4
EiRuMF <iuriemf@gmail.com>
2026-02-24 12:28:22 +0700

    Proile

--- inertia/pages/Profile/editProfile.tsx
+++ inertia/pages/Profile/editProfile.tsx
@@ -8,8 +8,14 @@ export default function EditProfile({ onEdit }: { onEdit: () => void }) {
         <h1>Edit Profile</h1>
         <ChevronRight />
       </CardContent>
+      <span className="mx-5 border-gray-400 border-b"></span>
       <CardContent className="font-semibold flex justify-between">
         <h1>Change Password</h1>
+        <ChevronRight />
+      </CardContent>
+      <span className="mx-5 border-gray-400 border-b"></span>
+      <CardContent className="font-semibold flex justify-between">
+        <h1>History Order</h1>
         <ChevronRight />
       </CardContent>
     </Card>
