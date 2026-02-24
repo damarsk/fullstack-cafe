b2d2609a7b150469f8690773371e0bdd25e93516
Damar S.K <damarsyahada12@gmail.com>
2026-02-24 12:26:28 +0700

    add pages menu detail, profile edit, change password, history

--- app/controllers/profiles_controller.ts
+++ app/controllers/profiles_controller.ts
@@ -4,4 +4,16 @@ export default class ProfilesController {
     public async index({ inertia }: HttpContext) {
         return inertia.render('Profile/index')
     }
+
+    public async changePassword({ inertia }: HttpContext) {
+        return inertia.render('Profile/changePassword')
+    }
+
+    public async history({ inertia }: HttpContext) {
+        return inertia.render('Profile/history')
+    }
+
+    public async edit({ inertia }: HttpContext) {
+        return inertia.render('Profile/edit')
+    }
 }
